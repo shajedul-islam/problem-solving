9d47276fd6bfd7c77d67995087c0db72a05a3947
Md. Shajedul Islam <shajedul.islam@robi.com.bd>
2019-06-10 21:04:10 +0600

    Solution to FrontBack problem

---
+++ src/FrontBackTest.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * Given a string, return a new string where the first and last chars have been exchanged.
+ *
+ *
+ * frontBack("code") → "eodc"
+ * frontBack("a") → "a"
+ * frontBack("ab") → "ba"
+ */
+
+public class FrontBackTest {
+    public static void main(String[] a) {
+        FrontBack frontBack = new FrontBack();
+        System.out.println(frontBack.frontBack("code"));
+        System.out.println(frontBack.frontBack("a"));
+        System.out.println(frontBack.frontBack("ab"));
+    }
+}
+
+class FrontBack {
+    public String frontBack(String str) {
+        return !str.isEmpty() && str.length() > 1 ?
+                str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0) : str;
+    }
+}
